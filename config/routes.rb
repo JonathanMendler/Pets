ef381a76e49b43c36958788086bdae4d6a2809e1
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-30 11:34:20 -0500

    Adds controller & routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/pets" => "pets#index"
+  post "/pets" => "pets#create"
+  get "/pets/:id" => "pets#show"
+  patch "pets/:id" => "pets#update"
+  delete "/pets/:id" => "pets#destroy"
 end
